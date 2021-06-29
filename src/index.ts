6e0621013135908372c4ed22d79be9887f337c17
sashafromlibertalia <floydreme@yandex.ru>
2021-06-30 00:28:04 +0300

    added exceptions + fixed algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
-/**
- * Default constants if custom values aren't declared.
- */
-const CONSTANTS = {
-    SMALL: 100,
-    BIG: 10000,
-    HUGE: 100000
+import { invalidArguments, wrongInterval } from "./exceptions"
+
+interface IRandozzier {
+    generate(start?: number, end?: number, step?: number): number
 }
 
-interface RandozzierValues {
-    generate(start?: number, end?: number, step?: number): number;
-}
-
-class Randozzier implements RandozzierValues {
+class Randozzier implements IRandozzier {
     constructor(private start?: number, private end?: number, private step?: number) {
-        this.start = 0
-        this.end = CONSTANTS.SMALL
+        this.start = 1
+        this.end = 100
         this.step = 1
     }
 
@@ -28,20 +21,31 @@ class Randozzier implements RandozzierValues {
      * @note Empty argument list returns random argument from [0, 100] with the step of 1.
      */
 
-    generate(start?: number, end?: number, step?: number): number {
+    generate(start?: number, end?: number, step?: number, options?: object): number {
         if (start != undefined && end != undefined && step != undefined) {
+            wrongInterval(start, end)
+
             this.start = start
             this.end = end
             this.step = step
+        } else {
+            invalidArguments(start, end, step)
         }
 
-        for (let iterator = this.start; iterator <= this.end; iterator += this.step) {
-            if (Math.round(this.DIVIDER / (iterator + Math.floor(Math.random() * this.step))) % 12 === 0) {
-                return iterator
-            } 
-            else {
-                return this.end - (Math.floor(Math.random() * 4) * this.step)
-            }
+        const m = this.end
+        const a = 1
+        const c = this.step
+        let arr: Array<number> = [this.start, this.start + this.step]
+        for (let iterator = 2; iterator < 100; iterator++) {
+            // Linear congruential generator
+            if ((a * arr[iterator - 1] + c) % m <= this.end)
+                arr[iterator] = (a * arr[iterator - 1] + c) % m
         }
+
+        return arr[Math.floor(Math.random() * arr.length)]
     }
 }
+
+
+const zier = new Randozzier()
+console.log(zier.generate(100, 200))
